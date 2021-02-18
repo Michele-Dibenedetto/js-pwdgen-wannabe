b38ebfad5d1d25dbee1699cf3ffd191eb99bc438
michele di benedetto <michele99cl@gmail.com>
2021-02-18 17:12:25 +0100

    esercizio completato

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// nome
+var name;
+name = prompt("inserisci il nome");
+console.log(name);
+
+// cognome
+var surname;
+surname = prompt("inserisci il cognome");
+console.log(surname);
+
+// color
+var color;
+color = prompt("inserisci il tuo colore preferito");
+console.log(color);
+
+// numero 19
+const number = 19;
+parseInt(number);
+
+// output password
+document.getElementById("profile").innerHTML=name + surname + color + number
